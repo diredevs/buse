87a968f5478647c563ad09bd59058bd5b4840fa9
luizrogeriocn <luizrogeriocn@gmail.com>
2013-10-02 07:29:22 -0300

    refactoring methods

--- views/js/mapOptions.js
+++ views/js/mapOptions.js
@@ -1,3 +1,33 @@
+var myPosition;
+var interval;
+var following;
+
+function markCurrentPosition() {
+	navigator.geolocation.getCurrentPosition(function(position) {
+		myPosition = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+	}, function() {});
+}
+
+function targetMe() {
+	markCurrentPosition();
+	map.setCenter(myPosition);
+}
+
+function followMe() {
+	targetMe();
+	map.setZoom(17);
+	map.setOptions({draggable: false});
+
+	interval = setInterval(function(){
+		if(following){
+			targetMe();
+		}
+		else{
+			clearInterval(interval);
+		}
+	}, 1000);
+}
+
 function enableMapDrag(){
 	map.setOptions({draggable: true});
 }
