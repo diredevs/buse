c3384c058301c95dd940fc3a33c6ab01834c485a
luizrogeriocn <luizrogeriocn@gmail.com>
2013-10-17 03:59:35 -0300

    Adding test frameworks fix#6

---
+++ test/bank-account.test.js
@@ -0,0 +1,10 @@
+// Require chai.js expect module for assertions
+var expect = require('chai').expect;
+
+// Create a new test suite for our Bank Account
+suite("Bank Account Tests", function() {
+
+    // Define a pending test
+    test("should exist");
+
+});
