aae7292dc982e8c5d790330510b5d7fdbcade016
Roger <luizrogeriocn@gmail.com>
2013-11-21 00:37:06 -0300

    Implementing RESTful API fixes #7

--- server.js
+++ server.js
@@ -20,12 +20,69 @@ var Schema = mongoose.Schema;
 var busSchema = new Schema({
   lat: Number,
   lng: Number,
-  id: Number,
+  bus_id: Number,
   accessibility: Boolean,
 });
 
 // define model =================
 var Bus = mongoose.model('Bus', busSchema);
+
+// routes ======================================================================
+
+// api ---------------------------------------------------------------------
+// get all todos
+app.get('/api/buses', function(req, res) {
+
+	// use mongoose to get all buses in the database
+	Bus.find(function(err, buses) {
+
+		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+		if (err)
+			res.send(err)
+
+		res.json(buses); // return all buses in JSON format
+	});
+});
+
+// create bus and send back all buses after creation
+app.post('/api/buses', function(req, res) {
+
+	// create a bus, information comes from AJAX request from Angular
+	Bus.create({
+		lat 			: req.body.lat,
+		lng 			: req.body.lng,
+		accessibility 	: req.body.accessibility
+
+	}, function(err, bus) {
+		if (err)
+			res.send(err);
+
+		// get and return all the buses after you create another
+		Bus.find(function(err, buses) {
+			if (err)
+				res.send(err)
+			res.json(buses);
+		});
+	});
+
+});
+
+// delete a bus
+app.delete('/api/buses/:bus_id', function(req, res) {
+	Bus.remove({
+		_id : req.params.bus_id
+	}, function(err, bus) {
+		if (err)
+			res.send(err);
+
+		// get and return all the buses after you delete another
+		Bus.find(function(err, buses) {
+			if (err)
+				res.send(err)
+			res.json(buses);
+		});
+	});
+});
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
