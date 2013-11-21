c60a7c888d67efd0e490281d49523056fa14ff1a
Roger <luizrogeriocn@gmail.com>
2013-11-21 00:11:31 -0300

    adding bus schema fixes #9

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-// server.js
+// set up ========================
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
 
-	// set up ========================
-	var express  = require('express');
-	var app      = express(); 								// create our app w/ express
-	var mongoose = require('mongoose'); 					// mongoose for mongodb
+// configuration =================
 
-	// configuration =================
+mongoose.connect('mongodb://<user>:<pass>@mongo.onmodulus.net:27017/ryte2jaG'); 	// connect to mongoDB database on modulus.io
 
-	mongoose.connect('mongodb://<user>:<pass>@mongo.onmodulus.net:27017/ryte2jaG'); 	// connect to mongoDB database on modulus.io
+app.configure(function() {
+	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+	app.use(express.logger('dev')); 						// log every request to the console
+	app.use(express.bodyParser()); 							// pull information from html in POST
+	app.use(express.methodOverride()); 						// simulate DELETE and PUT
+});
 
-	app.configure(function() {
-		app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-		app.use(express.logger('dev')); 						// log every request to the console
-		app.use(express.bodyParser()); 							// pull information from html in POST
-		app.use(express.methodOverride()); 						// simulate DELETE and PUT
-	});
+// define schema =================
+var Schema = mongoose.Schema;
 
-	// listen (start app with node server.js) ======================================
-	app.listen(8080);
-	console.log("App listening on port 8080");
+var busSchema = new Schema({
+  lat: double,
+  lng: double,
+  id: int,
+  accessibility: Boolean,
+});
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
