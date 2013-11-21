efc46c391fe06876a3b7e18ab92ef362ca205360
Roger <luizrogeriocn@gmail.com>
2013-11-21 00:41:49 -0300

    adding frontend route fixes #10

--- server.js
+++ server.js
@@ -84,6 +84,11 @@ app.delete('/api/buses/:bus_id', function(req, res) {
 	});
 });
 
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
