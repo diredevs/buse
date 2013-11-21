fc95bc9547f30a2b18fdc774a0a872dd7f1810ca
Roger <luizrogeriocn@gmail.com>
2013-11-20 23:48:39 -0300

    adding database. fixes #3

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+
+	// set up ========================
+	var express  = require('express');
+	var app      = express(); 								// create our app w/ express
+	var mongoose = require('mongoose'); 					// mongoose for mongodb
+
+	// configuration =================
+
+	mongoose.connect('mongodb://<user>:<pass>@mongo.onmodulus.net:27017/ryte2jaG'); 	// connect to mongoDB database on modulus.io
+
+	app.configure(function() {
+		app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+		app.use(express.logger('dev')); 						// log every request to the console
+		app.use(express.bodyParser()); 							// pull information from html in POST
+		app.use(express.methodOverride()); 						// simulate DELETE and PUT
+	});
+
+	// listen (start app with node server.js) ======================================
+	app.listen(8080);
+	console.log("App listening on port 8080");
